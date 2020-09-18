8832c7d3c02ce7b3dba658a337f7ae4a42052fc4
CitizenVP <petrusvasyl.j@gmail.com>
2020-09-18 21:34:32 +0300

    setup eslint and prettier and fix lint problem

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,11 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { DocumentContext } from 'next/document';
 
-export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return initialProps;
   }
 }
+
+export default MyDocument;
