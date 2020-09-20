3e6841d57564a5a3f080c0bdd8ef580ae126321a
CitizenVP <petrusvasyl.j@gmail.com>
2020-09-20 19:44:05 +0300

    create post pege

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-export function MainLayout({ children, title = 'Next App' }) {
+export default function MainLayout({ children, title = 'Next App' }) {
   return (
     <>
       <Head>
         <title>{title} | Next Course</title>
         <meta name="keywords" content="next,javascript,nextjs,react" />
-        <meta name="description" content="this is youtube tutorial for next" />
+        <meta name="description" content="" />
         <meta charSet="utf-8" />
       </Head>
       <nav>
---
+++ pages/posts.tsx
@@ -0,0 +1,65 @@
+import fetch from 'isomorphic-unfetch';
+import { NextPageContext } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+import Layout from '../components/Layout';
+import { MyPost } from '../interfaces/post';
+
+interface PostsPageProps {
+  posts: MyPost[];
+}
+
+export default function Posts({ posts: serverPosts }: PostsPageProps) {
+  const [posts, setPosts] = useState(serverPosts);
+
+  useEffect(() => {
+    async function load() {
+      const response = await fetch(`${process.env.API_URL}/posts`);
+      const json = await response.json();
+      setPosts(json);
+    }
+
+    if (!serverPosts) {
+      load();
+    }
+  }, []);
+
+  if (!posts) {
+    return (
+      <Layout>
+        <p>Loading ...</p>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>Posts Page | Next Course</title>
+      </Head>
+      <h1>Posts Page</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/post/[id]`} as={`/post/${post.id}`}>
+              <a>{post.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({ query, req }: PostNextPageContext) {
+  if (!req) {
+    return { posts: null };
+  }
+
+  const response = await fetch(`${process.env.API_URL}/posts`);
+  const posts = await response.json();
+
+  return { props: { posts } };
+}
